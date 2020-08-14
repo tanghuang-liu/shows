b85e0767ab2e369636a83ce3322d785fc853d010
tanghuang-liu <916650458@qq.com>
2020-08-14 18:09:20 +0800

    test

---
+++ src/views/dashboard/admin/coms/TPlc.vue
@@ -0,0 +1,42 @@
+<template>
+      <el-table
+              :data="tableData"
+              stripe
+              height="300px"
+              style="width: 100%">
+              <el-table-column
+                prop="date"
+                label="变量"
+               >
+              </el-table-column>
+              <el-table-column
+                prop="name"
+                label="类型"
+               >
+              </el-table-column>
+           </el-table>
+</template>
+<script>
+export default {
+    data(){
+        return {
+              tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }]
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -1,16 +1,92 @@
 <template>
-  <div>
-   fffff
+  <div class="pri">
+    <el-row :gutter="10">
+      <el-col :span="8">
+         <el-tabs type="border-card">
+          <el-tab-pane label="读取变量">
+            <T-plc/>
+          </el-tab-pane>
+          <el-tab-pane label="写入变量">配置管理</el-tab-pane>
+        </el-tabs>
+      </el-col>
+      <el-col :span="8">
+        <div class="head"><el-button type="text" icon="el-icon-caret-right">Run</el-button>
+        <el-button type="text" icon="el-icon-caret-right">Copy</el-button></div>
+      <el-card class="json-card" shadow="never">
+        {{test}}
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+         <div class="head"><h4>log</h4> </div>
+         <el-card class="log-card" shadow="never"></el-card>
+      </el-col>
+    </el-row>
+      <el-row style="margin-top:20px"> 
+            <el-card class="chart-card" shadow="never"></el-card>
+      </el-row>
   </div>
 </template>
 <script>
+import TPlc from './coms/TPlc'
 export default {
-  data(){
-    return{
-  
-    }
+  components:{
+     TPlc
   },
+    data() {
+      return {
+        test:'啦啦啦啦啦就\nkkkkkkkk',
+      };
+    },
   methods:{
+    
   }
 }
 </script>
+<style lang="scss" scoped>
+.pri{
+  background: #f2f2f2;
+  height: 100vh;
+  padding: 20px;
+}
+.head{
+  width: 100%;
+  height: 39px;
+  background: white;
+  border-bottom: 1px solid #dfe4ed;
+  padding-left: 15px;
+  h4{
+    margin: 0;
+    line-height: 39px;
+  }
+}
+.json-card{
+  height: 330px;
+  white-space: pre-wrap;
+}
+.log-card{
+  height: 330px;
+  white-space: pre-wrap;
+}
+.chart-card{
+  height: 450px;
+  white-space: pre-wrap;
+}
+
+
+</style>
+<style lang="less" scoped>
+/deep/.el-tabs--border-card{
+  border: none;
+  box-shadow: none;
+}
+/deep/.el-card{
+  border: none;
+  border-radius: 0;
+}
+/deep/.el-tabs__header{
+  background-color: white;
+}
+/deep/.el-table-column--selection .cell{
+  padding-left: 10px;
+}
+</style>
